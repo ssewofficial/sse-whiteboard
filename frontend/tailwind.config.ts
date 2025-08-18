a321a5fffebc39793ff1fcaefbd20b2c409bdc7e
SSE Official <sseworld4@gmail.com>
2025-08-18 18:40:30 +0530

    Remove unused context and utility files, including SecuritySetting, ThemeProvider, UserProvider, and various utility functions. Delete associated CSS and configuration files to streamline the project structure.

--- frontend/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  darkMode: "class",
-  content: ["./src/**.{ts,tsx,js,jsx}"],
-  theme: { extend: {} },
-  tailwindFunctions: ["tv"],
-} as Config;
